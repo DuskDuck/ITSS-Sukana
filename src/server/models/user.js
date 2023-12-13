c0343db39857b52b8ec4589bd912a8f86fc891d6
vu.nh1109 <vungohoang2001@gmail.com>
2023-12-13 23:30:56 +0700

    match chạy đúng logic

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -1,21 +1,79 @@
 import db from '../db/db.js';
 
 class User {
-  static async getAllUsersWithHobbies() {
+  static async getAllUsersWithHobbies({ gender, hobbies, city, minAge, maxAge }) {
     try {
-      const query = `
+      let sql = `
         SELECT users.*, GROUP_CONCAT(hobbies.name) as hobbies
         FROM users
         LEFT JOIN user_hobbies ON users.id = user_hobbies.user_id
         LEFT JOIN hobbies ON user_hobbies.hobby_id = hobbies.id
-        GROUP BY users.id;
       `;
 
-      const [usersWithHobbies] = await db.query(query);
+      const whereConditions = [];
+      let hobbiesConditions; // Declare the variable here
+
+      if (gender) {
+        whereConditions.push(`gender = '${gender}'`);
+      }
+
+      if (hobbies) {
+        const hobbiesArray = hobbies.split(',').map(Number);
+        hobbiesConditions = hobbiesArray.map(
+          hobbyId => `EXISTS (SELECT 1 FROM user_hobbies 
+            WHERE user_hobbies.user_id = users.id AND user_hobbies.hobby_id = ${hobbyId})`
+        );
+        whereConditions.push(`(${hobbiesConditions.join(' OR ')})`);
+      }
+
+      if (city) {
+        whereConditions.push(`city = '${city}'`);
+      }
+
+      if (minAge && maxAge) {
+        whereConditions.push(`age BETWEEN ${minAge} AND ${maxAge}`);
+      }
+
+      if (whereConditions.length > 0) {
+        sql += ' WHERE ' + whereConditions.join(' AND ');
+      }
+
+      sql += ' GROUP BY users.id';
+
+      if (hobbies) {
+        sql += ` ORDER BY COUNT(${hobbiesConditions.join(' OR ')}) DESC`;
+      }
+
+      const [usersWithHobbies] = await db.query(sql);
 
       return usersWithHobbies;
     } catch (error) {
       throw error;
+    }
+  }
+
+  static async getRandomUserNotInFriends(userId) {
+    try {
+        const [rows] = await db.query(`
+            SELECT u.*
+            FROM users u
+            WHERE u.id NOT IN (
+                SELECT f.receiver_id
+                FROM friends f
+                WHERE f.requester_id = ?
+                UNION
+                SELECT f.requester_id
+                FROM friends f
+                WHERE f.receiver_id = ?
+            )
+            AND u.id <> ?
+            ORDER BY RAND()
+            LIMIT 1;
+        `, [userId, userId, userId]);
+
+        return rows.length > 0 ? rows[0] : null;
+    } catch (error) {
+        throw error;
     }
   }
 }
--- src/server/routes/random_user.js
+++ src/server/routes/random_user.js
@@ -1,52 +1,24 @@
 import express from 'express';
-import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
-dotenv.config();
+import User from '../models/user.js';
 
 const router = express.Router();
-
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-});
 
 router.get('/api/match/:userId', async (req, res) => {
     try {
         const userId = parseInt(req.params.userId);
-        const [rows] = await connection.execute(`
-            SELECT u.*
-            FROM users u
-            WHERE u.id NOT IN (
-                SELECT f.receiver_id
-                FROM friends f
-                WHERE f.requester_id = ?
-                UNION
-                SELECT f.requester_id
-                FROM friends f
-                WHERE f.receiver_id = ?
-            )
-            AND u.id <> ?
-            ORDER BY RAND()
-            LIMIT 1;
-        `, [userId, userId, userId]);
-        if (rows.length === 0) {
+
+        // Get a random user who is not in the friend list of the given userId
+        const randomUser = await User.getRandomUserNotInFriends(userId);
+
+        if (!randomUser) {
             return res.status(404).json({ error: 'No matching user found' });
         }
-        const randomUser = rows[0];
+
         res.json(randomUser);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal server error' });
     }
 });
-try {
-    app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
-    });
-} catch (error) {
-    console.error('Error starting the server:', error);
-}
 
 export default router;
