4fd9813ca7e1ae55267bff75c7f53b366d38e03b
DuskDuck <nhuduc12754@gmail.com>
2023-12-19 22:39:25 +0700

    remove server inter

--- src/server/main.js
+++ src/server/main.js
@@ -1,21 +1,12 @@
-import express from 'express';
-import ViteExpress from 'vite-express';
-import db from './db/db.js';
-import userRoutes from './routes/users.js';
+import express from "express";
+import ViteExpress from "vite-express";
 
 const app = express();
 
-db.getConnection((err, connection) => {
-  if (err) {
-    console.error('Error connecting to MySQL:', err.message);
-  } else {
-    console.log('Connected to MySQL');
-    connection.release();
-  }
+app.get("/hello", (req, res) => {
+  res.send("Hello Vite + React!");
 });
 
-app.use('/api/users', userRoutes);
-
 ViteExpress.listen(app, 3000, () =>
-  console.log('Server is listening on port 3000...'),
+  console.log("Server is listening on port 3000..."),
 );
