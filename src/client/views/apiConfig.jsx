6808e6ddb222663aa88ed92e7def6445c5471941
DuskDuck <nhuduc12754@gmail.com>
2023-12-20 19:27:21 +0700

    fix deploy api endpoint

--- src/client/views/apiConfig.jsx
+++ src/client/views/apiConfig.jsx
@@ -1,6 +1,6 @@
 // apiConfig.js
 
 const isLocal = process.env.NODE_ENV=== 'development'; // Check if running in development mode
-const API_ENDPOINT = isLocal ? process.env.REACT_APP_LOCAL_API : process.env.REACT_APP_ONLINE_API;
+const API_ENDPOINT = isLocal ? process.env.REACT_APP_LOCAL_API : '';
 
 export default API_ENDPOINT;
