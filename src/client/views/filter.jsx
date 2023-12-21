2139f68505eeac51d8590bb956d8d3f891b219af
DuskDuck <nhuduc12754@gmail.com>
2023-12-21 10:30:38 +0700

    update axios module

--- src/client/views/filter.jsx
+++ src/client/views/filter.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import axios from "redaxios";
 import "./filter.css";
 import Slider from "@mui/material/Slider";
 import Divider from "@mui/material/Divider";
