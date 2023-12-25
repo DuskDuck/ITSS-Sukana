4bd65b0beca45e2532c0b617a38604746e9299f2
vu.nh1109 <vungohoang2001@gmail.com>
2023-12-25 23:18:23 +0700

    api lấy danh sách bạn bè

--- src/server/routes/friendRequests.js
+++ src/server/routes/friendRequests.js
@@ -13,7 +13,7 @@ router.post("/api/friends/send", async (req, res) => {
     await connection.beginTransaction();
 
     const [existingUsers] = await db.query(
-      `
+    `
       SELECT * FROM users WHERE id = ? OR id = ?
     `,
       [requester_id, receiver_id]
@@ -105,8 +105,8 @@ router.get("/api/friends/recieved/:user_id", async (req, res) => {
   try {
     const [existingUsers] = await db.query(
       `
-          SELECT * FROM users WHERE id = ?
-        `,
+        SELECT * FROM users WHERE id = ?
+      `,
       [user_id]
     );
 
@@ -115,20 +115,20 @@ router.get("/api/friends/recieved/:user_id", async (req, res) => {
     }
 
     const [friendRequests] = await db.execute(
-      `SELECT f.id, f.requester_id, f.receiver_id, f.status, f.created_at, 
-             u.first_name AS requester_first_name, u.last_name AS requester_last_name, i.url AS requester_filename
-             FROM friends f
-             JOIN users u ON f.requester_id = u.id
-             JOIN images i ON u.default_image_id = i.id
-             WHERE f.receiver_id = ? AND f.status = 'SENT'`,
-      [user_id]
+      `SELECT f.id, f.requester_id, f.receiver_id, f.status, f.created_at, f.updated_at,
+        u.first_name AS requester_first_name, u.last_name AS requester_last_name, i.url AS requester_filename
+        FROM friends f
+        JOIN users u ON f.requester_id = u.id
+        JOIN images i ON u.default_image_id = i.id
+        WHERE f.receiver_id = ? AND f.status = 'SENT'
+      `,[user_id]
     );
 
     const friendRequestsWithImages = await Promise.all(
       friendRequests.map(async (friend) => {
-        if (friend.requester_filename) {
-          friend.requester_image_url = await getImageURL(
-            friend.requester_filename
+        if (friend.friend_filename) {
+          friend.friend_image_url = await getImageURL(
+            friend.friend_filename
           );
         }
         return friend;
@@ -139,6 +139,56 @@ router.get("/api/friends/recieved/:user_id", async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Failed to fetch friend requests" });
+  }
+});
+
+router.get("/api/friends/user/:user_id", async (req, res) => {
+  const { user_id } = req.params;
+
+  try {
+    const [existingUsers] = await db.query(
+      `
+        SELECT * FROM users WHERE id = ?
+      `,
+      [user_id]
+    );
+
+    if (existingUsers.length === 0) {
+      return res.status(400).json({ error: "User does not exist!" });
+    }
+
+    const [friendList] = await db.execute(
+      `SELECT f.id, f.requester_id AS friend_id, f.status, f.created_at, f.updated_at,
+        u.first_name AS friend_first_name, u.last_name AS friend_last_name, i.url AS friend_filename
+        FROM friends f
+        JOIN users u ON f.requester_id = u.id
+        JOIN images i ON u.default_image_id = i.id
+        WHERE f.receiver_id = ? AND f.status = 'ACCEPTED'
+        UNION
+        SELECT f.id, f.receiver_id AS friend_id, f.status, f.created_at, f.updated_at,
+        u.first_name AS friend_first_name, u.last_name AS friend_last_name, i.url AS friend_filename
+        FROM friends f
+        JOIN users u ON f.receiver_id = u.id
+        JOIN images i ON u.default_image_id = i.id
+        WHERE f.requester_id = ? AND f.status = 'ACCEPTED'  
+      `,[user_id, user_id]
+    );
+
+    const friendListWithImages = await Promise.all(
+      friendList.map(async (friend) => {
+        if (friend.friend_filename) {
+          friend.friend_image_url = await getImageURL(
+            friend.friend_filename
+          );
+        }
+        return friend;
+      })
+    );
+
+    res.status(200).json({ friendListWithImages });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch friend list" });
   }
 });
 
