28d92ed8c59481b7de145662fd98bcad1c5b9719
damchua2001 <yourgitemail@example.com>
2023-12-26 21:13:17 +0700

    .

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
 import "./style.css";
 import Home from "./views/home";
@@ -16,6 +21,10 @@ const App = () => {
   return (
     <Router>
       <Routes>
+        <Route
+          path="/login"
+          element={<Login setIsLoggedIn={setIsLoggedIn} />}
+        />
         <Route path="/idealmatch" element={<Home />} />
         <Route path="/idealmatch-result" element={<Idealmatch />} />
         <Route path="/friend" element={<Friend />} />
@@ -30,7 +39,7 @@ const App = () => {
             isLoggedIn ? (
               <Homepage setIsLoggedIn={setIsLoggedIn} />
             ) : (
-              <Login setIsLoggedIn={setIsLoggedIn} />
+              <Navigate to="/login" />
             )
           }
         />
